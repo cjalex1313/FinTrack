638f6f2f937d86b3b45313e78513196526c23270
Alex Cioarec <cioarec.alexandru96@gmail.com>
2025-08-04 00:38:12 +0300

    Add health controller

---
+++ FinTrack/FinTrack.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using AcademyOS.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrack.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : BaseController
+{
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok();
+    }
+}
